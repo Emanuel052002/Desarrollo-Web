f99fb118bdc7e74d1bc37c48c527226e537a190c
Emanuel <caceres44028761@gmail.com>
2023-01-31 11:14:41 -0300

    terminamos comportamiento con JS

---
+++ JS/comportamiento.js
@@ -0,0 +1,17 @@
+
+
+document.getElementById('botonMagico1').addEventListener('click',function(){
+    document.getElementById('datosSobreMi').style.backgroundColor = '#ffe4c4'
+})
+
+document.getElementById('botonMagico2').addEventListener('click',function(){
+    document.getElementById('habilidades').style.backgroundColor = '#ffe4c4'
+})
+
+document.getElementById('botonMagico3').addEventListener('click',function(){
+    document.getElementById('experiencia').style.backgroundColor = '#ffe4c4'
+})
+
+document.getElementById('botonMagico4').addEventListener('click',function(){
+    document.getElementById('contacto').style.backgroundColor = '#ffe4c4'
+})
